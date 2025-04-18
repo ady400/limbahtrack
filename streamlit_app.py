d190561e34ef4116cc8af4e9afe6a7292ed33d8d
ady400 <raehan223@sma.belajar.id>
2025-04-18 19:05:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Set title and layout
 st.set_page_config(page_title="Pengolahan Limbah Industri", layout="wide")
