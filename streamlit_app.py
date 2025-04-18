affe94184b1e4d56b317d83cbe4fd5b8f830b103
ady400 <raehan223@sma.belajar.id>
2025-04-18 14:08:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ if menu == "Beranda":
    if menu == "Beranda":
     st.markdown('<h3 style="color: teal;">👋 Selamat Datang Mahasiswa politeknik AKA Bogor!</h3>', unsafe_allow_html=True)
     st.write("Aplikasi ini membantu kamu memahami dasar-dasar pengolahan limbah industri dengan cara yang interaktif.")
-    st.success(">" "Gunakan menu di samping untuk mulai belajar, menghitung COD, atau mengunggah laporan.")
+    st.success(">" "Gunakan menu di samping untuk mulai belajar, menghitung COD, atau mengunggah laporan." "<")
 # ---------- Halaman MATERI ----------
 elif menu == "Materi":
     st.subheader("📘 Materi Pembelajaran")
