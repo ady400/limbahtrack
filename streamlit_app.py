83ea165beb02ee55e64e27bd38f7de37b28c49f2
ady400 <raehan223@sma.belajar.id>
2025-04-26 09:26:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,26 +9,12 @@ menu = st.sidebar.radio("Menu", ["Beranda", "Proses", "Uji Lab", "Simulasi", "Te
 # BERANDA
 if menu == "Beranda":
     st.markdown('<div class="main-title">Aplikasi Pengolahan Limbah Industri</div>', unsafe_allow_html=True)
-    _LOREM_IPSUM = """
-Aplikasi ini dirancang untuk membantu memahami dan mensimulasikan pengolahan limbah industri secara sederhana dan interaktif.
-
-"""
-
+    _LOREM_IPSUM = """Aplikasi ini dirancang untuk membantu memahami dan mensimulasikan pengolahan limbah industri secara sederhana dan interaktif."""
 
 def stream_data():
     for word in _LOREM_IPSUM.split(" "):
         yield word + " "
         time.sleep(0.02)
-
-    yield pd.DataFrame(
-        np.random.randn(5, 10),
-        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-    )
-
-    for word in _LOREM_IPSUM.split(" "):
-        yield word + " "
-        time.sleep(0.02)
-
 
 if st.button("Stream data"):
     st.write_stream(stream_data)
