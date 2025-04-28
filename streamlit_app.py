f275ee3061188560bd1c3bb20873f5b111145c29
ady400 <raehan223@sma.belajar.id>
2025-04-28 09:57:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if menu == "Beranda":
     
 # PROSES
 if menu == "Proses":
-    st.markdown('<div class="main-title">⚙️ Tahapan Pengolahan Limbah Industri</div>', unsafe_allow_html=True)
+    st.header('<div class="main-title">⚙️ Tahapan Pengolahan Limbah Industri</div>', unsafe_allow_html=True)
     st.markdown("""
     ### 🧹 1. Pra-Pengolahan (Pre-Treatment)
     - *Screening:* Menyaring benda kasar seperti plastik dan kayu.
